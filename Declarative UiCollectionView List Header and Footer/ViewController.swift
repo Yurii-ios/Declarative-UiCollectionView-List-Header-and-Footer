9b8b06babf11dcdc98be9baa3edd0c7d336cc816
Yurii <Yurii>
2020-11-19 15:54:21 +0100

    Initial Commit

---
+++ Declarative UiCollectionView List Header and Footer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Declarative UiCollectionView List Header and Footer
+//
+//  Created by Yurii Sameliuk on 19/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
